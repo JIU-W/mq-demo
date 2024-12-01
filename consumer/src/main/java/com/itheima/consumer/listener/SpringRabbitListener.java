d7d76924398e007b270214b54782970afdcbdb41
JIU-W <2661727280@qq.com>
2024-12-01 13:01:30 +0800

    完整代码提交

---
+++ consumer/src/main/java/com/itheima/consumer/listener/SpringRabbitListener.java
@@ -0,0 +1,92 @@
+package com.itheima.consumer.listener;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.util.Map;
+
+@Component
+public class SpringRabbitListener {
+
+    // 利用RabbitListener来声明要监听的队列信息
+    // 将来一旦监听的队列中有了消息，就会推送给当前服务，调用当前方法，处理消息。
+    // 可以看到方法体中接收的就是消息体的内容
+    @RabbitListener(queues = "simple.queue")
+    public void listenSimpleQueueMessage(String msg) throws InterruptedException {
+        System.out.println("spring 消费者接收到消息：【" + msg + "】");
+    }
+
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue1(String msg) throws InterruptedException {
+        System.out.println("消费者1接收到消息：【" + msg + "】" + LocalTime.now());
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue2(String msg) throws InterruptedException {
+        System.err.println("消费者2........接收到消息：【" + msg + "】" + LocalTime.now());
+        Thread.sleep(200);
+    }
+
+    @RabbitListener(queues = "fanout.queue1")
+    public void listenFanoutQueue1(String msg) {
+        System.out.println("消费者1接收到Fanout消息：【" + msg + "】");
+    }
+
+    @RabbitListener(queues = "fanout.queue2")
+    public void listenFanoutQueue2(String msg) {
+        System.out.println("消费者2接收到Fanout消息：【" + msg + "】");
+    }
+
+    //@RabbitListener(queues = "direct.queue1")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "blue"}
+    ))
+    public void listenDirectQueue1(String msg) {
+        System.out.println("消费者1接收到direct.queue1的消息：【" + msg + "】");
+    }
+    
+    //@RabbitListener(queues = "direct.queue2")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "yellow"}
+    ))
+    public void listenDirectQueue2(String msg) {
+        System.out.println("消费者2接收到direct.queue2的消息：【" + msg + "】");
+    }
+
+    //@RabbitListener(queues = "topic.queue1")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue1"),
+            exchange = @Exchange(name = "hmall.topic", type = ExchangeTypes.TOPIC),
+            key = "china.#"
+    ))
+    public void listenTopicQueue1(String msg){
+        System.out.println("消费者1接收到topic.queue1的消息：【" + msg + "】");
+    }
+
+    //@RabbitListener(queues = "topic.queue2")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue2"),
+            exchange = @Exchange(name = "hmall.topic", type = ExchangeTypes.TOPIC),
+            key = "#.news"
+    ))
+    public void listenTopicQueue2(String msg){
+        System.out.println("消费者2接收到topic.queue2的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(queues = "object.queue")
+    public void listenSimpleQueueMessage(Map<String, Object> msg) throws InterruptedException {
+        System.out.println("消费者接收到object.queue消息：【" + msg + "】");
+    }
+
+}
